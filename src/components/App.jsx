f32de265e5f99c426d85d6da6cea5a78e089feed
ViktoriiaYakaba <yakaba@bk.ru>
2024-01-31 15:54:08 +0100

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,2 +1,22 @@
 import React from 'react';
-import Profile from './components/Profile';
+import Profile from './Profile';
+import userData from "./userData.json";
+
+const user = JSON.parse(JSON.stringify(userData));
+
+console.log(user);
+
+ const App = () => {
+  return (
+    <>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        image={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+};
+export default App;
